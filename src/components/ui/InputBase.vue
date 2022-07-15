1e79befb0b70f3c906beeb33dad7fe569426da3b
DmitriiPavlyuchenko <dorshichek1996@gmail.com>
2022-07-15 22:53:40 +0300

    Default inputBase

---
+++ src/components/ui/InputBase.vue
@@ -0,0 +1,26 @@
+<template>
+  <input :value="modelValue" class="input" @input="emitValue" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "InputBase",
+  props: {
+    modelValue: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ["update:modelValue"],
+  methods: {
+    emitValue(event: Event & { target: HTMLInputElement }) {
+      const value = event.target.value;
+      this.$emit("update:modelValue", value);
+    },
+  },
+});
+</script>
+
+<style scoped></style>
