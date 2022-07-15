2685b2ca72f2f6610359ec0bb75a07d88d5680b5
DmitriiPavlyuchenko <dorshichek1996@gmail.com>
2022-07-15 18:48:56 +0300

    Init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="test">
+    <HomePage></HomePage>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import "@/assets/styles/style.scss";
+import HomePage from "@/views/HomePage.vue";
+
+export default defineComponent({
+  components: { HomePage },
+});
+</script>
+
+<style lang="scss"></style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="test _icon-search">Hello World</div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style></style>
