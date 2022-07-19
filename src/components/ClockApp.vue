155758ffacca9a8013450a24174ef76aa201de18
DmitriiPavlyuchenko <dorshichek1996@gmail.com>
2022-07-19 11:17:17 +0300

    feature (ClockApp, DayPeriod) create clockApp, dayPeriod components, and add colors

---
+++ src/components/ClockApp.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="clock">
+    <h2 class="clock__title">Sunrise</h2>
+    <div class="clock__wrapper">
+      <div class="clock__arrows">
+        <span class="clock__arrow"></span>
+        <span class="clock__arrow"></span>
+        <span class="clock__arrow"></span>
+      </div>
+      <div class="clock__timers">
+        <span class="clock__time"></span>
+        <span class="clock__time"></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ClockApp",
+  props: {
+    isGoldenClock: {
+      type: Boolean,
+    },
+  },
+});
+</script>
+
+<style scoped></style>
---
+++ src/components/DayPeriodApp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="day-period">
+    <ClockApp></ClockApp>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import ClockApp from "@/components/ClockApp";
+
+export default defineComponent({
+  name: "DayPeriodApp",
+  components: { ClockApp },
+});
+</script>
+
+<style scoped></style>
