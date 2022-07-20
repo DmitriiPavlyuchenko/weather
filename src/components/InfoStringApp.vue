e747b72ea3f8c522e5bbc3ee0ce797f7175b78b2
DmitriiPavlyuchenko <dorshichek1996@gmail.com>
2022-07-20 12:24:49 +0300

    feature (info-string) added styles

---
+++ src/components/InfoStringApp.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="info-string">
+    <span class="info-string__string"></span>
+    <button class="info-string__info">i</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InfoStringApp",
+};
+</script>
+
+<style scoped></style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,16 +2,18 @@
   <div class="main">
     <HeaderApp></HeaderApp>
     <DayPeriodApp></DayPeriodApp>
+    <InfoStringApp></InfoStringApp>
   </div>
 </template>
 
 <script>
 import HeaderApp from "@/components/HeaderApp";
 import DayPeriodApp from "@/components/DayPeriodApp";
+import InfoStringApp from "@/components/InfoStringApp";
 
 export default {
   name: "MainPage",
-  components: { HeaderApp, DayPeriodApp },
+  components: { InfoStringApp, HeaderApp, DayPeriodApp },
 };
 </script>
 <style scoped></style>
