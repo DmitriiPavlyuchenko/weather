b1f435a08c416f2fe49e06a75c38b984a6226a23
DmitriiPavlyuchenko <dorshichek1996@gmail.com>
2022-07-22 16:40:33 +0300

    Features (theme) added theme swiper

--- src/components/SwitchesApp.vue
+++ src/components/SwitchesApp.vue
@@ -1,11 +1,36 @@
 <template>
-  <div class=""></div>
+  <div class="switcher-theme" @click="changeTheme">
+    <span class="switcher-theme__purple switcher-options"></span>
+    <span class="switcher-theme__blue switcher-options"></span>
+    <span class="switcher-theme__red switcher-options"></span>
+  </div>
 </template>
 
 <script>
-export default {
+import { defineComponent } from "vue";
+
+export default defineComponent({
   name: "SwitchesApp",
-};
+  components: {},
+  data() {
+    return {
+      theme: "purple",
+    };
+  },
+  methods: {
+    changeTheme(event) {
+      let element = event.target.classList;
+      if (element.contains("switcher-theme__purple")) {
+        this.theme = "purple";
+      } else if (element.contains("switcher-theme__blue")) {
+        this.theme = "blue";
+      } else if (element.contains("switcher-theme__red")) {
+        this.theme = "red";
+      }
+      this.$emit("change", this.theme);
+    },
+  },
+});
 </script>
 
 <style scoped></style>
